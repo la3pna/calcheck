d0a45c90350f974d5b125320a19e3848d37ecc12
la3pna <la3pna@gmail.com>
2013-08-07 09:12:53 +0200

    better part of s-par import in place

--- calcheck/calcheck/Form1.cs
+++ calcheck/calcheck/Form1.cs
@@ -46,15 +46,15 @@ namespace calcheck
             {
                 if (openFileDialog1.ShowDialog() == DialogResult.OK)
                 {
-                    string line;           
+                    string line;
                     int i = 0;
                     List<double> freq = new List<double>();
                     List<Complex> s11a = new List<Complex>();
                     List<Complex> s21a = new List<Complex>();
                     List<Complex> s22a = new List<Complex>();
                     List<Complex> s12a = new List<Complex>();
-                
-                  
+
+
 
 
                     System.IO.StreamReader sr = new
@@ -70,19 +70,30 @@ namespace calcheck
                         {
                             def = line;
                             string[] defstrn = line.Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
-                            
-                             freqBase = defstrn[1];
-                             paramtype = defstrn[2];
-                             measure = defstrn[3];
+
+                            freqBase = defstrn[1];
+                            paramtype = defstrn[2];
+                            measure = defstrn[3];
                             Z0 = defstrn[5];
 
-                            textBox5.Text = Z0; 
+                            textBox5.Text = Z0;
+
+                            if (paramtype != "S")
+                            {
+                                label2.Text = "PARAMETER NOT S-PARAMETERS!";
+                            }
 
                         }
+
                         else
                         {
 
-                            string[] servalspl = line.Split( new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries );
+                            Complex s11b;
+                            Complex s21b;
+                            Complex s12b;
+                            Complex s22b;
+
+                            string[] servalspl = line.Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
 
                             string freqStrn = servalspl[0];
                             string s11reStrn = servalspl[1];
@@ -104,34 +115,60 @@ namespace calcheck
                             float s22imag = Convert.ToSingle(s11imagStrn, CultureInfo.InvariantCulture);
                             float s12re = Convert.ToSingle(s11reStrn, CultureInfo.InvariantCulture);
                             float s12imag = Convert.ToSingle(s11imagStrn, CultureInfo.InvariantCulture);
-                           
-                            
+
+                            if (measure == "MA")
+                            {
+                                //convert to cartesian
+                                s11b = Complex.FromPolarCoordinates(s11re, s11imag * Math.PI / 180);
+                                s12b = Complex.FromPolarCoordinates(s12re, s12imag * Math.PI / 180);
+                                s21b = Complex.FromPolarCoordinates(s21re, s21imag * Math.PI / 180);
+                                s22b = Complex.FromPolarCoordinates(s22re, s22imag * Math.PI / 180);
+                                //Complex c1 = Complex.FromPolarCoordinates(10, 45 * Math.PI / 180)
+                            }
+
+                            if (measure == "RI")
+                            {
+                                //leave as is
+                                s11b = new Complex(s11re, s11imag);
+                                s21b = new Complex(s21re, s21imag);
+                                s22b = new Complex(s22re, s22imag);
+                                s12b = new Complex(s12re, s12imag);
+                            }
+
+
+                            if (measure == "DB")
+                            {
+                                
+                                // 10^db/20 for each real part. then got polar.
+                                s11b = Complex.FromPolarCoordinates( Math.Pow(10, (s11re/20)), s11imag * Math.PI / 180);
+                                s12b = Complex.FromPolarCoordinates( Math.Pow(10, (s12re/20)), s12imag * Math.PI / 180);
+                                s21b = Complex.FromPolarCoordinates( Math.Pow(10, (s21re/20)), s21imag * Math.PI / 180);
+                                s22b = Complex.FromPolarCoordinates( Math.Pow(10, (s22re/20)), s22imag * Math.PI / 180);
+                            }
+
                             freq.Add(freqa);
-
-                            Complex s11b = new Complex(s11re, s11imag);
-                            Complex s21b = new Complex(s21re, s21imag);
-                            Complex s22b = new Complex(s22re, s22imag);
-                            Complex s12b = new Complex(s12re, s12imag);
-
                             s11a.Add(s11b);
                             s12a.Add(s12b);
                             s21a.Add(s21b);
                             s22a.Add(s22b);
                             i = i + 1;
+
                         }
+
+
                         vectorLength = i;
-                        
+
                     }
-                   
+
                     sr.Close();
 
-                    Complex[] s11 = s11a.ToArray();
-                    Complex[] s21 = s21a.ToArray();
-                    Complex[] s12 = s12a.ToArray();
-                    Complex[] s22 = s22a.ToArray();
-                    convertVector();
+                    s11 = s11a.ToArray();
+                    s21 = s21a.ToArray();
+                    s12 = s12a.ToArray();
+                    s22 = s22a.ToArray();
                 }
-            }
+                }
+            
             catch (Exception ex)
             {
                textBox2.Text= ("The file could not be read:");
@@ -145,43 +182,10 @@ namespace calcheck
 
         }
 
-        private void convertVector()
-        {
-            if (paramtype != "S")
-            {
-                label2.Text = "PARAMETER NOT S-PARAMTERS!";
-            }
+      
+            
 
-            if (measure == "MA")
-            {
-
-            }
-
-            if (measure == "RI")
-            {
-                
-            }
-            if (measure == "DB")
-            {
-               
-                // 10^db/20 for each real part. then got polar.
-            }
-
-
-            /*
-             * Complex s11b = new Complex(s11re, s11imag);
-               Complex s21b = new Complex(s21re, s21imag);
-               Complex s22b = new Complex(s22re, s22imag);
-               Complex s12b = new Complex(s12re, s12imag);
-             * 
-             *  Complex[] s11 = s11a.ToArray();
-                    Complex[] s21 = s21a.ToArray();
-                    Complex[] s12 = s12a.ToArray();
-                    Complex[] s22 = s22a.ToArray();
-              
-             */
-
-        }
+        
 
     }
 }
